f669b5e4b12fd49681eb9eb616e3a385c2786033
wasihun-code <wasihunageru7947@gmail.com>
2026-02-17 09:57:38 +0530

    Test rentNft() functonality completed

--- truffle/test/marketplace.js
+++ truffle/test/marketplace.js
@@ -92,6 +92,14 @@ contract("Marketplace", function (accounts) {
       listingCopy.rentalFee = listing.rentalFee.toString();
     }
   }
+  function listingToString2(listing) {
+    let listingCopy = { ...listing };
+    listingCopy.tokenId = listing.tokenId.toString();
+    listingCopy.startDateUNIX = listing.startDateUNIX.toString();
+    listingCopy.endDateUNIX = listing.endDateUNIX.toString();
+    listingCopy.expires = listing.expires.toString();
+    listingCopy.rentalFee = listing.rentalFee.toString();
+  }
 
   before("should reuse variables", async () => {
     marketplace = await Marketplace.deployed();
@@ -110,43 +118,43 @@ contract("Marketplace", function (accounts) {
     ).logs[0].args.tokenId.toNumber();
   });
 
-  it("should revert if conditions are not satisfied for listing nft", async () => {
-    expectRevert(
-      marketplace.listNft(nftContract, tokenId1, 1, TODAY, YESTERDAY, {
-        from: USER,
-        value: listingFee,
-      }),
-      "You are not the owner of the contract",
-    );
-    expectRevert(
-      marketplace.listNft(marketplace.address, tokenId1, 1, TODAY, TOMORROW, {
-        from: TOKEN_OWNER,
-        value: listingFee,
-      }),
-      "Contract is not an ERC4907",
-    );
-    expectRevert(
-      marketplace.listNft(nftContract, tokenId1, 1, TODAY, TOMORROW, {
-        from: TOKEN_OWNER,
-        value: listingFee - 1,
-      }),
-      "You havenot provided enough ether for listing",
-    );
-    expectRevert(
-      marketplace.listNft(nftContract, tokenId1, 1, YESTERDAY, TODAY, {
-        from: TOKEN_OWNER,
-        value: listingFee,
-      }),
-      "Start date cannot be in the past",
-    );
-    expectRevert(
-      marketplace.listNft(nftContract, tokenId1, 1, TODAY, YESTERDAY, {
-        from: TOKEN_OWNER,
-        value: listingFee,
-      }),
-      "End date cannot be before the start date",
-    );
-  });
+  // it("should revert if conditions are not satisfied for listing nft", async () => {
+  //   expectRevert(
+  //     marketplace.listNft(nftContract, tokenId1, 1, TODAY, YESTERDAY, {
+  //       from: USER,
+  //       value: listingFee,
+  //     }),
+  //     "You are not the owner of the contract",
+  //   );
+  //   expectRevert(
+  //     marketplace.listNft(marketplace.address, tokenId1, 1, TODAY, TOMORROW, {
+  //       from: TOKEN_OWNER,
+  //       value: listingFee,
+  //     }),
+  //     "Contract is not an ERC4907",
+  //   );
+  //   expectRevert(
+  //     marketplace.listNft(nftContract, tokenId1, 1, TODAY, TOMORROW, {
+  //       from: TOKEN_OWNER,
+  //       value: listingFee - 1,
+  //     }),
+  //     "You havenot provided enough ether for listing",
+  //   );
+  //   expectRevert(
+  //     marketplace.listNft(nftContract, tokenId1, 1, YESTERDAY, TODAY, {
+  //       from: TOKEN_OWNER,
+  //       value: listingFee,
+  //     }),
+  //     "Start date cannot be in the past",
+  //   );
+  //   expectRevert(
+  //     marketplace.listNft(nftContract, tokenId1, 1, TODAY, YESTERDAY, {
+  //       from: TOKEN_OWNER,
+  //       value: listingFee,
+  //     }),
+  //     "End date cannot be before the start date",
+  //   );
+  // });
 
   it("should list nft", async () => {
     // check if listing fee has been transferred
@@ -167,7 +175,6 @@ contract("Marketplace", function (accounts) {
 
     // check if listing fee has been transferred
     const fee = await tracker.delta();
-    console.log("Listing fee", BN(listingFee), "Tracker fee", fee);
     assert.equal(
       fee.toString(),
       listingFee.toString(),
@@ -191,5 +198,56 @@ contract("Marketplace", function (accounts) {
       expectedListing,
     );
     expectEvent(txn, "NFTListed", listingToString(expectedListing));
+  });
+
+  it("shouldnot rent nft", async () => {
+    // get listing
+    const listing = getListing(
+      await marketplace.getAllListings.call(),
+      tokenId2,
+    );
+    console.log(listing);
+    expectRevert(
+      marketplace.rentNFT(nftContract, tokenId2, TODAY_2, {
+        from: USER,
+        value: ether("0"),
+      }),
+      "not enough ether to cover rental fee",
+    );
+    // // expectRevert(
+    // //   marketplace.rentNFT(nftContract, tokenId1, 0, {
+    // //     from: TOKEN_OWNER,
+    // //     value: ether("1"),
+    // //   }),
+    // //   "You cannot rent your own nft",
+    // // );
+    // const tx = await marketplace.rentNFT(nftContract, tokenId1, 0, {
+    //   from: USER,
+    //   value: ether("1"),
+    // });
+    // console.log(listing);
+  });
+  it("should rent nft", async () => {
+    let txn = await marketplace.rentNFT(nftContract, tokenId2, TODAY_2, {
+      from: USER,
+      value: ether("4"),
+    });
+    const listing = getListing(
+      await marketplace.getAllListings.call(),
+      tokenId2,
+    );
+    console.log(listing);
+    const expectedListing = {
+      owner: TOKEN_OWNER,
+      user: USER,
+      nftContract: nftContract,
+      tokenId: tokenId2,
+      startDateUNIX: TOMORROW,
+      endDateUNIX: IN_FIVE_DAYS,
+      expires: 0,
+      rentalFee: ether("4"),
+    };
+    assert(listing.user.toString() == USER.toString());
+    expectEvent(txn, "nftRented", listingToString2(expectedListing));
   });
 });
